c6a88e46e1dfc399a00d632aeeac505b5058a95b
jeonDev <jjhwhdgus0727@gmail.com>
2025-05-25 21:42:43 +0900

    feat ( exception ) : Global Exception 처리 추가

---
+++ src/main/java/com/jeon/market/common/exception/GlobalException.java
@@ -0,0 +1,25 @@
+package com.jeon.market.common.exception;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@Hidden // swagger 충돌로 인해 추가
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> validException(final MethodArgumentNotValidException e) {
+        log.error("[Valid] Endpoint 검증 오류 : {}", e.getMessage());
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ErrorResponse.of("V0", message)
+                );
+    }
+}
